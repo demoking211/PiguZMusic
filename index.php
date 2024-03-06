0ddb435542a4e580035f901365551887a61ac36d
Mingze <mingez18.2002@gmail.com>
2024-03-07 01:29:50 +0800

    File Adjustment
    - Separate homepage css to index.css

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/index.css">
         <?php require 'includes/cdn_linker.php'?>
 
         <title>PiguZMusic - Homepage</title>
