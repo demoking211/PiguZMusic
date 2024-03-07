fab374b0cc2ac2d8d69b73ff89050d4f19e1db49
Mingze <mingez18.2002@gmail.com>
2024-03-07 17:57:32 +0800

    Pricelist page done

---
+++ pricelist.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="8-UTF">
+    <meta name = "Viewport" content=""width="device-width", initial scale="1.0">
+    <link rel="stylesheet" href="css\pricelist.css">
+    <link rel="stylesheet" href="css\main.css">
+    <?php require 'includes/cdn_linker.php';?>
+    <title>PiguZMusic - Subscription Plan</title> 
+    </head>   
+    <body>
+        <div class="price_pg container-fluid"> 
+            <h2 class="text-center"><b>USER PRICING PLAN</b></h2>
+            <div class ="price-row">
+                <div class = "price-col">
+                    <p>Standard</p>
+                    <div class="price_box">
+                        <div><span>RM</span>10.00</div>
+                        <div>/MONTHLY</div>
+                    </div>
+                    <ul class="feature_bar list-unstyled text-start">
+                        <li><i class="fa fa-check"></i><span>Ads Free</span></li>
+                        <li><i class="fa fa-check"></i><span>Unlmited Upload Spaces</span></li>
+                        <li><i class="fa fa-times"></i><span>Free Usage</span></li>
+                        <li><i class="fa fa-times"></i><span>Allow quality solution</span></li>
+                        <li><i class="fa fa-times"></i><span>Offline Listening Plan</span></li>
+                    </ul>
+                    <button><i class = "fa fa-check"></i></button>
+                </div>
+                <div class = "price-col">
+                    <p>Premium</p>
+                    <div class="price_box">
+                        <div><span>RM</span>30.00</div>
+                        <div>/MONTHLY</div>
+                    </div>
+                    <ul class="feature_bar list-unstyled text-start">
+                        <li><i class="fa fa-check"></i><span>Ads Free</span></li>
+                        <li><i class="fa fa-check"></i><span>Unlmited Upload Spaces</span></li>
+                        <li><i class="fa fa-check"></i><span>Free Usage</span></li>
+                        <li><i class="fa fa-check"></i><span>Allow quality solution</span></li>
+                        <li><i class="fa fa-check"></i><span>Offline Listening Plan</span></li>
+                    </ul>
+                    <button>PURCHASE</button>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
